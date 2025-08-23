5d5e836c681b3047001af4bec5cb8dc658426606
poornachandra-sarasAI <mohan.poornachandra01@sarasai.org>
2025-08-24 03:07:46 +0530

    Provided access to frontend deployed origin

--- app/main.py
+++ app/main.py
@@ -4,11 +4,17 @@ from app.routes import router
 
 app = FastAPI(title="AI Video Generator")
 
+
+origins = [
+    "http://localhost:5000",
+    "https://videocrafter.vercel.app/",
+    "http://localhost:3000"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # later restrict to your frontend domain
+    allow_origins=origins,  # later restrict to your frontend domain
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 app.include_router(router)
