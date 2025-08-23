3f3c3611e30efb79cb6011fcba55bf4553d2dcf2
poornachandra-sarasAI <mohan.poornachandra01@sarasai.org>
2025-08-24 02:09:11 +0530

    Added CORS to FastAPI

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import router
 
 app = FastAPI(title="AI Video Generator")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later restrict to your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
